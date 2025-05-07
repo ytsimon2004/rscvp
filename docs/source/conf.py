e4900280567e67beb1f0957981906d5cc8ca9f0f
Yu-Ting Wei <ytsimon2004@gmail.com>
2025-05-07 15:41:50 +0200

    fix build pipeline

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,10 +1,15 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../../src'))
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
 
 project = 'rscvp'
 copyright = '2025, yu-ting wei'
