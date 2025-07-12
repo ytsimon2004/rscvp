e5f8fcb941f039953fb21a0236104b0f3fc30473
Yu-Ting Wei <ytsimon2004@gmail.com>
2025-07-12 15:36:39 +0200

    add slice transform view

--- src/rscvp/atlas/main_slice_transform.py
+++ src/rscvp/atlas/main_slice_transform.py
@@ -5,101 +5,95 @@ import cv2
 import numpy as np
 from scipy.io import loadmat
 
+from argclz import AbstractParser, argument, as_argument, str_tuple_type
 from neuralib.atlas.ccf.matrix import load_transform_matrix
 from neuralib.atlas.typing import PLANE_TYPE
 from neuralib.imglib.transform import apply_transformation
 from neuralib.plot import plot_figure
 from neuralib.typing import PathLike
-from neuralib.util.unstable import unstable
+from neuralib.util.verbose import publish_annotation
+from rscvp.util.cli import HistOptions
 
-# {P: (W, H)}
+__all__ = ['SliceTransformOptions', 'slice_transform_helper']
+
 DEFAULT_DIMENSION: dict[PLANE_TYPE, tuple[int, int]] = {
     'coronal': (1140, 800),
     'sagittal': (1320, 800)
 }
+"""{P: (W, H)} for 10 um mouse atlas"""
 
 
-@unstable()
-def plot_slice_view_transform(raw_image: PathLike,
-                              trans_matrix: np.ndarray | PathLike,
-                              annotation: PathLike,
-                              plane_type: PLANE_TYPE,
-                              *,
-                              resize_dim: tuple[int, int] | None = None,
-                              flip_lr_raw: bool = True,
-                              flip_ud_raw: bool = False,
-                              output: PathLike | None = None):
-    """
+@publish_annotation('appendix', project='rscvp', as_doc=True, caption='check slice transform and expression')
+class SliceTransformOptions(AbstractParser, HistOptions):
+    DESCRIPTION = 'Plot the 1) orig image 2) overlay transformed image + boundary 3) annotation brain region'
 
-    :param raw_image: raw brain slice
-    :param trans_matrix: .mat file transformation array, note that `t` while using 2dccf matlab `save` func
-                (due to `projective2d` seems runtime call,
-                thus not save in transformation.mat, need to save separately)
-    :param plane_type: slice cutting orientation
-    :param resize_dim: image down-sampling (W, H). Predefine by allen mouse atlas in default settings
-    :param annotation: provide the .mat file after the 2dccf registration
-    :param flip_lr_raw: horizontally flip the raw image. i.e, matching the transformation matrix
-    :param flip_ud_raw: vertically flip the raw image. i.e, matching the transformation matrix
-    :param output: image output
-    :return:
-    """
+    animal = as_argument(HistOptions.animal).with_options(required=False)
 
+    raw_image: Path = argument('-R', '--raw', help='raw image path')
+    trans_matrix: Path = argument('-T', '--trans', help='transform matrix path (3 x 3)')
+    ccf_matrix: Path = argument('--ccf', help='ccf matrix .mat file')
+
+    annotation_region: tuple[str, ...] = argument(
+        '--annotation',
+        type=str_tuple_type,
+        default=('LD',),
+        help='annotation brain region'
+    )
+    overlay_only: bool = argument('--overlay', help='only show image')
+
+    def run(self):
+        raw, trans = slice_transform_helper(self.raw_image, self.trans_matrix, plane_type=self.cut_plane)
+        x, y = DEFAULT_DIMENSION[self.cut_plane]
+        extent = (-x / 2, x / 2, -y / 2, y / 2)
+
+        if self.overlay_only:
+            with plot_figure(None) as ax:
+                ax.imshow(trans, extent=extent)
+
+                matrix = load_transform_matrix(self.ccf_matrix, self.cut_plane)
+                plane = matrix.get_slice_plane()
+                plane._plot_boundaries(ax=ax, extent=extent, cmap='binary_r', alpha=0.7)
+
+        else:
+            with plot_figure(None, 1, 3) as ax:
+                ax[0].imshow(raw)
+                ax[1].imshow(trans, extent=extent)
+
+                matrix = load_transform_matrix(self.ccf_matrix, self.cut_plane)
+                plane = matrix.get_slice_plane()
+                plane._plot_boundaries(ax=ax[1], extent=extent, cmap='binary_r', alpha=0.7)
+                plane.plot(ax=ax[2], annotation_region=list(self.annotation_region), extent=extent, boundaries=True)
+
+
+def slice_transform_helper(raw_image: PathLike,
+                           trans_matrix: np.ndarray | PathLike, *,
+                           plane_type: PLANE_TYPE = 'coronal',
+                           flip_lr: bool = False,
+                           flip_ud: bool = False) -> tuple[np.ndarray, np.ndarray]:
     raw_image = cv2.cvtColor(cv2.imread(raw_image), cv2.COLOR_BGR2RGB)
 
-    #
-    if flip_ud_raw:
+    if flip_ud:
         raw_image = np.flipud(raw_image)
 
-    if flip_lr_raw:
+    if flip_lr:
         raw_image = np.fliplr(raw_image)
 
-    #
-    if resize_dim is None:
-        resize = cv2.resize(raw_image, DEFAULT_DIMENSION[plane_type])
-    else:
-        resize = cv2.resize(raw_image, resize_dim)
-
-    #
     if isinstance(trans_matrix, get_args(PathLike)):
         s = Path(trans_matrix).suffix
         if s == '.mat':
-            trans_matrix = loadmat(trans_matrix)['t'].T
+            mtx = loadmat(trans_matrix)['t'].T
         elif s == '.npy':
-            trans_matrix = np.load(trans_matrix)
+            mtx = np.load(trans_matrix)
         else:
             raise ValueError(f'invalid file type for the transformation matrix: {s}')
+    else:
+        mtx = trans_matrix
 
-    trans = apply_transformation(resize, trans_matrix)
+    resize = cv2.resize(raw_image, DEFAULT_DIMENSION[plane_type])
+    transform_image = apply_transformation(resize, mtx)
 
-    with plot_figure(output, 1, 2) as ax:
-        x, y = DEFAULT_DIMENSION[plane_type]
-        extent = (-x / 2, x / 2, -y / 2, y / 2)
-        ax[0].imshow(raw_image)
-        ax[1].imshow(trans, extent=extent)
-
-        #
-        matrix = load_transform_matrix(annotation, plane_type)
-        matrix.get_slice_plane()._plot_boundaries(ax=ax[1], extent=extent, cmap='binary_r', alpha=1)
-
-
-def main():
-    import argparse
-
-    ap = argparse.ArgumentParser()
-    ap.add_argument('-R', '--raw', metavar='FILE', type=Path, help='raw image', dest='raw')
-    ap.add_argument('-T', '--transform', metavar='FILE', type=Path, help='transformation matrix array', dest='trans')
-    ap.add_argument('-M', metavar='FILE', type=Path, help='transformation matrix', dest='mat')
-    ap.add_argument('-P', '--plane', choices=get_args(PLANE_TYPE), help='plane type', dest='plane')
-    ap.add_argument('-O', '--output', metavar='FILE', type=Path, help='figure output path', dest='output')
-
-    opt = ap.parse_args()
-
-    plot_slice_view_transform(raw_image=opt.raw,
-                              trans_matrix=opt.trans,
-                              annotation=opt.mat,
-                              plane_type=opt.plane,
-                              output=opt.output)
+    return raw_image, transform_image
 
 
 if __name__ == '__main__':
-    main()
+    SliceTransformOptions().main()
