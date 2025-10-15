15cd9d26523a296c401635d32ba54a93b7285425
Yu-Ting Wei <ytsimon2004@gmail.com>
2025-10-15 17:44:01 +0200

    save

--- bash/vop/batch_persistence.sh
+++ bash/vop/batch_persistence.sh
@@ -1,14 +1,35 @@
+#! /bin/bash
 
+set -e
 
-# ==================== #
-# Sorted Position Bins #
-# ==================== #
+anterior_persistence() {
+  local m=$1
+  shift 1
+  sleep 10
 
-
-python -m rscvp.statistic.persistence_agg.main_trial_avg_position \
+  python -m rscvp.behavioral.$m \
   -D 210315,210401,210402,210416,210604,210519,211202,211203,211202,221018 \
   -A YW006,YW006,YW008,YW008,YW010,YW017,YW022,YW032,YW033,YW048 \
   -P 0,0,0,0,0,0,,,, \
+  "$@"
+}
+
+posterior_persistence() {
+  local m=$1
+  shift 1
+  sleep 10
+
+  python -m rscvp.statistic.persistence_agg.$m \
+  -D 210402,210409,210407,210409,210610,210514,211209,211208,211208,221019 \
+  -A YW006,YW006,YW008,YW008,YW010,YW017,YW022,YW032,YW033,YW048 \
+  -P 0,0,0,0,0,0,,,, \
+  "$@"
+}
+
+
+# ------- Sorted Position Bins ---------- #
+
+anterior_persistence main_trial_avg_position \
   --region aRSC,aRSC,aRSC,aRSC,aRSC,aRSC,aRSC,aRSC,aRSC,aRSC \
   --group \
   --as-group 0,0,0,0,0,0,0,0,0,0 \
@@ -19,10 +40,7 @@ python -m rscvp.statistic.persistence_agg.main_trial_avg_position \
   --page ap_place
 
 
- python -m rscvp.statistic.persistence_agg.main_trial_avg_position \
-  -D 210402,210409,210407,210409,210610,210514,211209,211208,211208,221019 \
-  -A YW006,YW006,YW008,YW008,YW010,YW017,YW022,YW032,YW033,YW048 \
-  -P 0,0,0,0,0,0,,,, \
+posterior_persistence main_trial_avg_position \
   --region pRSC,pRSC,pRSC,pRSC,pRSC,pRSC,pRSC,pRSC,pRSC,pRSC \
   --group \
   --as-group 1,1,1,1,1,1,1,1,1,1 \
@@ -31,3 +49,16 @@ python -m rscvp.statistic.persistence_agg.main_trial_avg_position \
   --sort light \
   -T spks \
   --page ap_place
+
+
+# ------- Motion mismatch ---------- #
+
+anterior_persistence main_mismatch
+
+anterior_persistence main_mismatch \
+  --paired-group ctrl
+
+posterior_persistence main_mismatch
+
+posterior_persistence main_mismatch \
+  --paired-group ctrl
