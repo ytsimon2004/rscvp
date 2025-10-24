dbda8f9a7411b614ff0571b1744b4ee947d7646f
Yu-Ting Wei <ytsimon2004@gmail.com>
2025-10-24 10:35:55 +0200

    clean persistence auto cli uage

--- bash/ldl/batch_persistence.sh
+++ bash/ldl/batch_persistence.sh
@@ -1,13 +1,36 @@
+#! /bin/bash
 
-# ==================== #
-# Sorted Position Bins #
-# ==================== #
+set -e
 
+anterior_persistence() {
+  local m=$1
+  shift 1
+  sleep 10
 
-python -m rscvp.statistic.persistence_agg.main_trial_avg_position \
+  python -m rscvp.behavioral.$m \
   -D 211210,220322,211207,220325,220901,221216 \
   -A YW022,YW033,YW032,YW036,YW045,YW048 \
   -P ,,,,0, \
+  "$@"
+}
+
+posterior_persistence() {
+  local m=$1
+  shift 1
+  sleep 10
+
+  python -m rscvp.statistic.persistence_agg.$m \
+  -D 220902,221215,230113 \
+  -A YW045,YW048,YW049 \
+  -P ,, \
+  "$@"
+}
+
+
+
+# ------- Sorted Position Bins ---------- #
+
+anterior_persistence main_trial_avg_position \
   --region aRSC,aRSC,aRSC,aRSC,aRSC,aRSC \
   --group \
   --as-group 0,0,0,0,0,0 \
@@ -18,10 +41,7 @@ python -m rscvp.statistic.persistence_agg.main_trial_avg_position \
   --page ap_ldl
 
 
-python -m rscvp.statistic.persistence_agg.main_trial_avg_position \
-  -D 220902,221215,230113 \
-  -A YW045,YW048,YW049 \
-  -P ,, \
+posterior_persistence main_trial_avg_position \
   --region pRSC,pRSC,pRSC \
   --group \
   --as-group 1,1,1 \
--- bash/vop/batch_persistence.sh
+++ bash/vop/batch_persistence.sh
@@ -27,6 +27,20 @@ posterior_persistence() {
 }
 
 
+all_persistence() {
+  local m=$1
+  shift 1
+  sleep 10
+
+  python -m rscvp.statistic.persistence_agg.$m \
+  -D 210315,210401,210402,210416,210604,210519,211202,211203,211202,221018,210402,210409,210407,210409,210610,210514,211209,211208,211208,221019 \
+  -A YW006,YW006,YW008,YW008,YW010,YW017,YW022,YW032,YW033,YW048,YW006,YW006,YW008,YW008,YW010,YW017,YW022,YW032,YW033,YW048 \
+  -P 0,0,0,0,0,0,,,,0,0,0,0,0,0,,,, \
+  "$@"
+
+}
+
+
 # ------- Sorted Position Bins ---------- #
 
 anterior_persistence main_trial_avg_position \
@@ -39,7 +53,6 @@ anterior_persistence main_trial_avg_position \
   -T spks \
   --page ap_place
 
-
 posterior_persistence main_trial_avg_position \
   --region pRSC,pRSC,pRSC,pRSC,pRSC,pRSC,pRSC,pRSC,pRSC,pRSC \
   --group \
@@ -49,6 +62,115 @@ posterior_persistence main_trial_avg_position \
   --sort light \
   -T spks \
   --page ap_place
+
+# ------- Alignment Position Bins Sort SI---------- #
+
+anterior_persistence main_si_sorted_occ \
+  --top 500
+
+posterior_persistence main_si_sorted_occ \
+  --top 500
+
+
+# ------- Decoding confusion matrix ---------- #
+
+anterior_persistence main_decode_err \
+  --region aRSC,aRSC,aRSC,aRSC,aRSC,aRSC,aRSC,aRSC,aRSC,aRSC \
+  --type confusion_matrix \
+  --CV odd \
+  --random 200 \
+  --session light \
+  --used_session light \
+  -w 100 \
+  --page ap_place
+
+anterior_persistence main_decode_err \
+  --region aRSC,aRSC,aRSC,aRSC,aRSC,aRSC,aRSC,aRSC,aRSC,aRSC \
+  --type confusion_matrix \
+  --group \
+  --as-group 0,0,0,0,0,0,0,0,0,0 \
+  --CV odd \
+  --random 200 \
+  --session light \
+  --used_session light \
+  -w 100 \
+  --page ap_place
+
+posterior_persistence main_decode_err \
+  --region pRSC,pRSC,pRSC,pRSC,pRSC,pRSC,pRSC,pRSC,pRSC,pRSC \
+  --type confusion_matrix \
+  --CV odd \
+  --random 200 \
+  --session light \
+  --used_session light \
+  -w 100 \
+  --page ap_place
+
+posterior_persistence main_decode_err \
+  --region pRSC,pRSC,pRSC,pRSC,pRSC,pRSC,pRSC,pRSC,pRSC,pRSC \
+  --type confusion_matrix \
+  --group \
+  --as-group 0,0,0,0,0,0,0,0,0,0 \
+  --CV odd \
+  --random 200 \
+  --session light \
+  --used_session light \
+  -w 100 \
+  --page ap_place
+
+
+# ------- Decoding position bins ---------- #
+
+anterior_persistence main_decode_err \
+  --region aRSC,aRSC,aRSC,aRSC,aRSC,aRSC,aRSC,aRSC,aRSC,aRSC \
+  --type position_bins_error \
+  --CV odd \
+  --random 200 \
+  --session light \
+  --used_session light \
+  -w 100 \
+  --page ap_place
+
+anterior_persistence main_decode_err \
+  --region aRSC,aRSC,aRSC,aRSC,aRSC,aRSC,aRSC,aRSC,aRSC,aRSC \
+  --type position_bins_error \
+  --group \
+  --as-group 0,0,0,0,0,0,0,0,0,0 \
+  --CV odd \
+  --random 200 \
+  --session light \
+  --used_session light \
+  -w 100 \
+  --page ap_place
+
+posterior_persistence main_decode_err \
+  --region pRSC,pRSC,pRSC,pRSC,pRSC,pRSC,pRSC,pRSC,pRSC,pRSC \
+  --type position_bins_error \
+  --CV odd \
+  --random 200 \
+  --session light \
+  --used_session light \
+  -w 100 \
+  --page ap_place
+
+posterior_persistence main_decode_err \
+  --region pRSC,pRSC,pRSC,pRSC,pRSC,pRSC,pRSC,pRSC,pRSC,pRSC \
+  --type position_bins_error \
+  --group \
+  --as-group 0,0,0,0,0,0,0,0,0,0 \
+  --CV odd \
+  --random 200 \
+  --session light \
+  --used_session light \
+  -w 100 \
+  --page ap_place
+
+
+# ------- Topographical cell type ---------- #
+
+all_persistence  main_topo_celltype \
+  --smooth 3 \
+  --ctype spatial
 
 
 # ------- Motion mismatch ---------- #
