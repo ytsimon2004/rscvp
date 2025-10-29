2918247e26b3e42bd2dc0a97994b476b2fb31473
Yu-Ting Wei <ytsimon2004@gmail.com>
2025-10-29 11:51:50 +0100

    add debug print rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,6 +2,13 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('../../src'))
+import traceback
+
+try:
+    import rscvp.util.cli
+except Exception as e:
+    print("####### Import of rscvp.util.cli failed:", e)
+    traceback.print_exc()
 
 # Configuration file for the Sphinx documentation builder.
 #
